c821187f038604031c685ed4e71a7ac87aa8f048
Kritika Ganesh Tripathi <kritikaktripathi@gmail.com>
2021-01-21 20:42:34 +0530

    Fibnocci Again

--- Ds-Questions/fibnocciNormally.js
+++ Ds-Questions/fibnocciNormally.js
@@ -5,6 +5,7 @@ function runProgram( input ) {
 
     for (let i = 2; i <= input; i++) {
         arr.push(arr[arr.length - 1] + arr[arr.length - 2])
+        // arr[arr.length - 1] = arr[arr.length - 2] = arr[2-1] => arr[1] + arr[0]
     }
     console.log(arr[arr.length - 1]) // to print the last
     // console.log(arr) // to print the series
