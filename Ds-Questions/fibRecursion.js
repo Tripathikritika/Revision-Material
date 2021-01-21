686b3d3223c8c130ff69b98523c449e5bd826795
Kritika Ganesh Tripathi <kritikaktripathi@gmail.com>
2021-01-21 20:43:07 +0530

    Fib Recursion Again

--- Ds-Questions/fibRecursion.js
+++ Ds-Questions/fibRecursion.js
@@ -1,23 +1,44 @@
+// function runProgram( input ) {
+//     input = Number(input)
+    
+//     for( let i = 0 ; i <= input ; i++){
+//         console.log(fib( i ))
+        
+//     }
+
+//     function fib (n) {
+        
+//         if( n === 0 ){
+//             return n
+//         }
+//         else if ( n === 1){
+//             return n
+//         }
+//         else{
+//             return fib(n-1) + fib(n-2)
+//         }
+//     }
+// }
+// runProgram( 10 )
+
 function runProgram( input ) {
     input = Number(input)
-    let res = 0
-    
-    for( let i = 0 ; i <= input ; i++){
-        console.log(fib( res ))
-        res++
+
+    for( let i = 0 ; i < input ; i ++){
+        let res = fib(i)
+        console.log(res)
     }
 
-    function fib (n) {
-        
-        if( n === 0 ){
+    function fib( n ){
+        if( n === 0){
             return n
         }
-        else if ( n === 1){
+        else if( n === 1){
             return n
         }
         else{
-            return fib(n-1) + fib(n-2)
+            return fib(n-2) + fib(n-1)
         }
     }
 }
-runProgram( 10 )
+runProgram( 5 )
