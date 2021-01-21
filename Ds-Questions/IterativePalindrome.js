d9d949971587bca9cdf99b815039f012a5d5bf49
Kritika Ganesh Tripathi <kritikaktripathi@gmail.com>
2021-01-21 20:44:06 +0530

    Palindrome again

--- Ds-Questions/IterativePalindrome.js
+++ Ds-Questions/IterativePalindrome.js
@@ -1,15 +1,37 @@
-function runProgram( paliValue ){
-    paliValue = paliValue.split("")
-    let len = paliValue.length
+// function runProgram( paliValue ){
+//     paliValue = paliValue.split("")
+//     let len = paliValue.length
 
-    for( let i = 0 ; i < Math.floor(len/2) ; i++){
+//     for( let i = 0 ; i < Math.floor(len/2) ; i++){
        
-        if( paliValue[i] !== paliValue[len-1-i]){
+//         if( paliValue[i] !== paliValue[len-1-i]){
+//             console.log("No")
+//             return
+//         }
+
+//     }
+//     console.log("yes")
+// }
+// runProgram( "madam")
+
+function runProgram( str ){
+    str = str.split("")
+    for( let i = 0 ; i < Math.floor(str.length)/2 ; i++){
+        if(str[i] != str[str.length-i-1]){
             console.log("No")
             return
         }
-
     }
-    console.log("yes")
+    console.log("Yes")
 }
 runProgram( "madam")
+
+/*
+0 1 2 3 4
+m a d a m
+
+str[0] !== str[5-0-1] => str[0] != str[4] => m != m
+str[1] !== str[5-1-1] => str[1] != str[3] => a != a
+d
+therefore its a palindrome
+*/
