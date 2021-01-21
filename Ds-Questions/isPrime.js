6cbf87ea8eb90a8375214a40ca506677b3dfc538
Kritika Ganesh Tripathi <kritikaktripathi@gmail.com>
2021-01-21 20:43:37 +0530

    Prime or not

--- Ds-Questions/isPrime.js
+++ Ds-Questions/isPrime.js
@@ -1,25 +1,55 @@
+// arr = [1,3,5,4,8,13,11,15,21,23].map(Number)
+// function runProgram( arr ) {
+//     let res = []
+//     for( let i = 0 ; i < arr.length ; i++ ){
+//        if(checkPrime(arr[i])){
+//            res.push(1)
+//        }
+//        else{
+//            res.push(0)
+//        }
+//     }
+//     console.log(res)
+//     function checkPrime( num ) {
+//         if( num === 1 ){
+//             return false 
+//         }
+//         else if( num === 2){
+//             return true
+//         }
+//         else{
+//             for( let j = 2 ; j < num ; j++){
+//                 if( num % j == 0){
+//                     return false
+//                 }
+//             }
+//             return true
+//         }
+//     }
+// }
+// runProgram(arr)
+
 arr = [1,3,5,4,8,13,11,15,21,23].map(Number)
 function runProgram( arr ) {
-    let res = []
-    for( let i = 0 ; i < arr.length ; i++ ){
-       if(checkPrime(arr[i])){
-           res.push(1)
+    
+   for( let i = 0 ; i < arr.length ; i++){
+       if(isPrime(arr[i])){
+            console.log(arr[i])
        }
        else{
-           res.push(0)
+            console.log("Not prime")
        }
     }
-    console.log(res)
-    function checkPrime( num ) {
-        if( num === 1 ){
-            return false 
+    function isPrime( n ){
+        if( n === 1){
+            return false
         }
-        else if( num === 2){
+        else if( n === 2){
             return true
         }
         else{
-            for( let j = 2 ; j < num ; j++){
-                if( num % j == 0){
+            for( let i = 2 ; i < n ; i++){
+                if( n % i === 0){
                     return false
                 }
             }
